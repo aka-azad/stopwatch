e4bd0a37b52d3a92f00f8415d23fae58a38053a8
aka-azad <ashrafazad86@gmail.com>
2023-01-09 00:59:19 +0600

    create stopwatch

---
+++ main.js
@@ -0,0 +1,44 @@
+const timeEl = document.querySelector( ".time");
+const startBtn = document.getElementById("start");
+const stopBtn = document.getElementById("stop");
+const resetBtn = document.getElementById("reset");
+
+let seconds = 00;
+let interval = null;
+
+startBtn.addEventListener('click', start)
+stopBtn.addEventListener('click', stop)
+resetBtn.addEventListener('click', reset)
+
+function timer() {
+  seconds++;
+  let hrs = Math.floor(seconds / (60 * 60));
+  let mins = Math.floor((seconds -(hrs * 3600)) / 60);
+  let secs = seconds % 60;
+
+  if (secs < 10) secs = '0' + secs
+  if (mins < 10) mins = '0' + mins
+  if (hrs < 10) hrs = '0' + hrs
+  
+  timeEl.innerText = `${hrs}:${mins}:${secs}`
+
+}
+
+let x =0 ;
+function start() {
+  if (interval) {
+    return
+  }
+  interval = setInterval(timer, 1000)
+}
+
+function stop() {
+  clearInterval(interval)
+  interval = null
+}
+
+function reset () {
+  stop()
+  seconds = -1
+  timer()
+}
